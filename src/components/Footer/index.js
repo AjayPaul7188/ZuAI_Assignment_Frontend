995ec8e8ef0396cf28783ac9be58ae91896b8e0d
AjayPaul7188 <ajaypaulmummdi@gmail.com>
2024-08-23 16:52:35 +0530

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.css'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className='footer'>
+                <p>© 2024 Blog App</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
